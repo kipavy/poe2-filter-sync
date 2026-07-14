ba38a9ea8e27db94932f0abefb0c59aaaf6487a3
kipavy <kiki.kalagan@gmail.com>
2026-07-14 14:48:05 +0200

    test: decouple SoundMapping tests from production config

--- tests/SoundMapping.Tests.ps1
+++ tests/SoundMapping.Tests.ps1
@@ -2,25 +2,45 @@ BeforeAll {
     Import-Module "$PSScriptRoot/../src/SoundMapping.psm1" -Force
     $script:soundsDir = Join-Path $TestDrive 'sounds'
     New-Item -ItemType Directory -Path $script:soundsDir | Out-Null
-    foreach ($f in '1maybevaluable.mp3','2currency.mp3','3uniques.mp3','4maps.mp3','5highmaps.mp3','6veryvaluable.mp3','gay-echo.mp3','TRIPLE_MONSTRE.mp3') {
+    foreach ($f in 'myA.mp3','myB.mp3') {
         New-Item -ItemType File -Path (Join-Path $script:soundsDir $f) | Out-Null
     }
+
     $script:cfgPath = Join-Path $TestDrive 'sound-mapping.json'
-    Copy-Item "$PSScriptRoot/../config/sound-mapping.json" $script:cfgPath
+    $fixture = [ordered]@{
+        poeFilterId       = ''
+        poeFilterName     = 'Test Filter'
+        sourceFilterMatch = '3-STRICT'
+        followUrls        = @('https://example.test/f')
+        fileReplacements  = [ordered]@{
+            '1maybevaluable.mp3' = 'myA.mp3'
+        }
+        targetedOverrides = @(
+            [ordered]@{
+                identifier = '$type->currency $tier->d'
+                file       = 'myB.mp3'
+                volume     = 250
+            }
+        )
+    }
+    $fixture | ConvertTo-Json -Depth 5 | Set-Content -Path $script:cfgPath
 }
 
 Describe 'Get-SoundMapping' {
     It 'parses metadata and sections' {
         $m = Get-SoundMapping -Path $script:cfgPath -SoundsDir $script:soundsDir
-        $m.PoeFilterName | Should -Be 'NeverSink + custom sounds'
+        $m.PoeFilterName | Should -Be 'Test Filter'
         $m.SourceFilterMatch | Should -Be '3-STRICT'
-        $m.TargetedOverrides.Count | Should -Be 2
-        $m.TargetedOverrides[0].Volume | Should -Be 300
+        $m.FollowUrls[0] | Should -Be 'https://example.test/f'
+        $m.FollowUrls.Count | Should -Be 1
+        $m.TargetedOverrides.Count | Should -Be 1
+        $m.TargetedOverrides[0].Volume | Should -Be 250
+        $m.FileReplacements['1maybevaluable.mp3'] | Should -Be 'myA.mp3'
     }
     It 'throws when a referenced sound file is missing' {
-        Remove-Item (Join-Path $script:soundsDir 'gay-echo.mp3')
+        Remove-Item (Join-Path $script:soundsDir 'myB.mp3')
         { Get-SoundMapping -Path $script:cfgPath -SoundsDir $script:soundsDir } |
-            Should -Throw -ExpectedMessage '*gay-echo.mp3*'
+            Should -Throw -ExpectedMessage '*myB.mp3*'
     }
     It 'throws when the config file path does not exist' {
         { Get-SoundMapping -Path (Join-Path $TestDrive 'nonexistent.json') -SoundsDir $script:soundsDir } |
